224e33898f7a981aedeffa5cea96aea6577a582a
Anatolii Fenin <fenintolja@gmail.com>
2024-01-24 14:19:37 +0200

    setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav className=" border-2 border-red-500 flexBetween max-container padding-container relative z-30">
+        Navbar
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
