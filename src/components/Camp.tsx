aa12dc05dc2f56897fefe1cc5533294d5c889cc9
Anatolii Fenin <fenintolja@gmail.com>
2024-01-26 13:57:55 +0200

    camp section

--- src/components/Camp.tsx
+++ src/components/Camp.tsx
@@ -1,5 +1,97 @@
+/* eslint-disable react/no-unescaped-entities */
+import { PEOPLE_URL } from '@/constants';
+import { Span } from 'next/dist/trace';
+import Image from 'next/image';
+
+interface ICampSiteProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: ICampSiteProps) => {
+  return (
+    <div
+      className={` h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className=" h-full flex flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className=" flexCenter gap-4">
+          <div className=" rounded-full p-4 bg-green-50">
+            <Image src="folded-map.svg" alt="map" width={28} height={28} />
+          </div>
+          <div className="flex flex-col gap-1 text-white regular-14">
+            <h4 className="bold-18">{title}</h4>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((person) => (
+              <Image
+                key={person}
+                src={person}
+                alt="person"
+                width={52}
+                height={52}
+                className="inline-block h-10 w-10 rounded-full"
+              />
+            ))}
+          </span>
+          <span className="text-white bold-16 md:bold-20">{peopleJoined}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className=" 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          backgroundImage="bg-bg-img-2"
+          title="Mountain View Camp"
+          subtitle="Somewhere in the Wilderness"
+          peopleJoined="60+ Joined"
+        />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="text-white regular-24 md:regular-32 2xl:regular-64 capitalize">
+            <strong>Feeling Lost </strong>
+            And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image
+            src="quote.svg"
+            alt="quote"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
