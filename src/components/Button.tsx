32b14dbae112aef6d24d4b1cd4c42278c58760e7
Anatolii Fenin <fenintolja@gmail.com>
2024-01-29 12:04:22 +0200

    get app section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,19 @@ import { ButtonHTMLAttributes } from 'react';
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   icon?: string;
-  variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text';
+  variant:
+    | 'btn_dark_green'
+    | 'btn_green'
+    | 'btn_white_text'
+    | 'btn_white'
+    | 'btn_dark_green_outline';
 }
 
 const Button = ({ title, icon, variant, ...props }: IButtonProps) => {
   return (
     <button
       {...props}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${props.className}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <span>{title}</span>
